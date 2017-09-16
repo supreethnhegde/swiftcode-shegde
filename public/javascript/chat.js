e5f2e2482da76dc7bf94c0874599468be57777cc
Live session user <supreethnh55@gmail.com>
2017-09-16 07:52:38 +0000

    final ui

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,5 +1,27 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController',  function ($scope){
+	$scope.messages = [
+	{
+		sender: "BOT",
+		text: "what can i do for you?",
+		time: "1:12 pm"
+	},
+	{
+		sender: "USER",
+		text: "what is 1+1",
+		time: "1:13pm"
+	},
+	{
+		sender: "USER",
+		text: "who are you",
+		time: "1:14pm"
+	},
+	];
+	var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+	exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);    
+	};	       
 
 });
